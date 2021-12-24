2554c33fe2d18f6a25e3d2130542ed60f6706b6d
Hamed Rezaie Saniani <Hamed.r.Saniani@outlook.com>
2021-12-24 18:35:21 +0330

    Create CourseTests Class and Write First Test( test that Course Constructor Construct Course Properly Or Not) - is this "Happy Path" ( path that we not test Exceptions and have no Error)

--- Academy.Domain.Tests/CourseTests.cs
+++ Academy.Domain.Tests/CourseTests.cs
@@ -2,22 +2,22 @@ using Xunit;
 
 namespace Academy.Domain.Tests
 {
-    //public class CourseTests
-    //{
-    //    [Fact]
-    //    public void Constructor_ShouldConstructCourseProperly()
-    //    {
-    //        const int id = 1;
-    //        const string name = "Tdd & Bdd";
-    //        const bool isOnline = true;
-    //        const double tuition = 4000;
+    public class CourseTests
+    {
+        [Fact]
+        public void Constructor_ShouldConstructCourseProperly()
+        {
+            const int id = 1;
+            const string name = "Tdd & Bdd";
+            const bool isOnline = true;
+            const double tuition = 4000;
 
-    //        var course = new Course(id,name,isOnline,tuition);
+            var course = new Course(id, name, isOnline, tuition);
 
-    //        Assert.Equal(id, course.Id);
-    //        Assert.Equal(name, course.Name);
-    //        Assert.Equal(isOnline, course.IsOnline);
-    //        Assert.Equal(tuition, course.Tuition);
-    //    }
-    //}
+            Assert.Equal(id, course.Id);
+            Assert.Equal(name, course.Name);
+            Assert.Equal(isOnline, course.IsOnline);
+            Assert.Equal(tuition, course.Tuition);
+        }
+    }
 }
