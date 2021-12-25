78254030cb663a532e7eeb1766d5b3e53db6dcef
Hamed Rezaie Saniani <Hamed.r.Saniani@outlook.com>
2021-12-25 23:24:10 +0330

    Test Exceptions _ Refactor Project

--- Academy.Domain.Tests/CourseTests.cs
+++ Academy.Domain.Tests/CourseTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Xunit;
 
 namespace Academy.Domain.Tests
@@ -18,6 +19,37 @@ namespace Academy.Domain.Tests
             Assert.Equal(name, course.Name);
             Assert.Equal(isOnline, course.IsOnline);
             Assert.Equal(tuition, course.Tuition);
+        }
+        [Fact]
+        public void Constructor_ShouldThrowException_When_NameIsNotProvided()
+        {
+            const int id = 1;
+            const string name = "";
+            const bool isOnline = true;
+            const double tuition = 4000;
+
+            //Action course = () => new Course(id, name, isOnline, tuition);
+            void course() => new Course(id, name, isOnline, tuition);
+
+            Assert.Throws<Exception>((Action) course);
+        }
+        [Fact]
+        public void Constructor_ShouldThrowException_When_TuitionIsNotProvided()
+        {
+            const int id = 1;
+            const string name = "Tdd & Bdd";
+            const bool isOnline = true;
+            double tuition = 0;
+
+            Action course = () => new Course(id, name, isOnline, tuition);
+            //void course() => new Course(id, name, isOnline, tuition);
+
+            tuition = -1;
+
+            Action course2 = () => new Course(id, name, isOnline, tuition);
+
+            Assert.Throws<ArgumentOutOfRangeException>(course);
+            Assert.Throws<ArgumentOutOfRangeException>(course2);
         }
     }
 }
---
+++ Academy.Domain/Course.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Academy.Domain
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; internal set; }
+        public bool IsOnline { get; internal set; }
+        public double Tuition { get; internal set; }
+        public Course(int id, string name, bool isOnline, double tuition)
+        {
+            CheckNameIsNull(name);
+            CheckTuitionIsLessOrEqualZero(tuition);
+            Id = id;
+            Name = name;
+            IsOnline = isOnline;
+            Tuition = tuition;
+        }
+
+        private static void CheckTuitionIsLessOrEqualZero(double tuition)
+        {
+            if (tuition <= 0)
+                throw new ArgumentOutOfRangeException();
+        }
+
+        private static void CheckNameIsNull(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new Exception();
+        }
+    }
+}
