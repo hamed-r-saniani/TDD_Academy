2bd8855f9a5fa227054db4383def96e298059147
Hamed Rezaie Saniani <Hamed.r.Saniani@outlook.com>
2022-01-26 23:07:41 +0330

    Test Get & GetById Methods  { Repository }

--- Academy.Infrastructure.Tests.Unit/CourseRepositoryTests.cs
+++ Academy.Infrastructure.Tests.Unit/CourseRepositoryTests.cs
@@ -7,22 +7,70 @@ namespace Academy.Infrastructure.Tests.Unit
 {
     public class CourseRepositoryTests
     {
+        private readonly CourseRepository _courseRepository;
+        private readonly CourseTestBuilder _courseTestBuilder;
+
+        public CourseRepositoryTests()
+        {
+            _courseRepository = new CourseRepository();
+            _courseTestBuilder = new CourseTestBuilder();
+        }
+
         [Fact]
         public void Should_AddNewCourseToCoursesList()
         {
             //arrange
-            var courseBuilder = new CourseTestBuilder();
-            var course = courseBuilder.Build();
-            var repository = new CourseRepository();
+            var course = _courseTestBuilder.Build();
 
             //act
-            repository.Create(course);
+            _courseRepository.Create(course);
 
             //assert
             //repository.Courses.Should().ContainEquivalentOf(course); //Test Equal of MembersName of Two Object and Then MembersValues of Two Object // if two object come from different point this method is uses and we want check MembersValues of Two Object
-            repository.Courses.Should().Contain(course); //Can Override For Better Performance // Test Reference Of Two Object that Does not work in Database Repository but Work in Local DataStoring // Call Equal Method of Object
+            _courseRepository.Courses.Should().Contain(course); //Can Override For Better Performance // Test Reference Of Two Object that Does not work in Database Repository but Work in Local DataStoring // Call Equal Method of Object
 
             // Speed Of Tests is Important to us
+        }
+
+        [Fact]
+        public void Should_ReturnListOfCourses()
+        {
+            //arrange
+            //var courseRepository = new CourseRepository();
+
+            //act
+            var courses = _courseRepository.GetAll();
+
+            //assert
+            courses.Should().HaveCountGreaterThanOrEqualTo(0);
+        }
+
+        [Fact]
+        public void Should_ReturnCourseById()
+        {
+            //arrange
+            const int id = 4;
+            var expectedCourse = _courseTestBuilder.WithId(id).Build();
+            _courseRepository.Create(expectedCourse);
+
+            //act
+            var actual = _courseRepository.GetBy(id);
+
+            //assert
+            actual.Should().Be(expectedCourse);
+        }
+
+        [Fact]
+        public void Should_ReturnNull_WhenIdNotExists()
+        {
+            //arrange
+            const int id = 300;
+
+            //act
+            var actual = _courseRepository.GetBy(id);
+
+            //assert
+            actual.Should().BeNull();
         }
     }
 }
--- Academy.Infrastructure/CourseRepository.cs
+++ Academy.Infrastructure/CourseRepository.cs
@@ -1,5 +1,7 @@
 ﻿using Academy.Domain;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Academy.Infrastructure.Tests.Unit
 {
@@ -9,10 +11,20 @@ namespace Academy.Infrastructure.Tests.Unit
         {
             new Course(30,"Asp.Net Core",true,4444,"Hamed Rezaie Saniani")
         };
-         
+
         public void Create(Domain.Course course)
         {
             Courses.Add(course);
+        }
+
+        public List<Course> GetAll()
+        {
+            return Courses;
+        }
+
+        public Course GetBy(int id)
+        {
+            return Courses.FirstOrDefault(x => x.Id == id);
         }
     }
 }
